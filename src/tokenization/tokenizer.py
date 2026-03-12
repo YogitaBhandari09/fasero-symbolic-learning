2f1f23cec62643bc2b10ab276d07a559ed799505
Yogita Bhandari <yogitabhandari0901@gmail.com>
2026-03-13 00:53:34 +0530

    Add tokenizer for symbolic expressions

--- src/tokenization/tokenizer.py
+++ src/tokenization/tokenizer.py
@@ -1,1 +1,14 @@
+import re
 
+def tokenize(expr):
+
+    tokens = re.findall(r'\d+|[a-zA-Z]+|\*\*|\S', expr)
+
+    return tokens
+
+
+if __name__ == "__main__":
+
+    expr = "x - x**3/6"
+
+    print(tokenize(expr))
