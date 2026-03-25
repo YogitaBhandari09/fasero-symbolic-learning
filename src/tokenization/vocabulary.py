b7c009118a60903bfde720e83366150292267efa
Yogita Bhandari <yogitabhandari0901@gmail.com>
2026-03-25 22:18:46 +0530

    Add SymPy dataset generator

--- src/tokenization/vocabulary.py
+++ src/tokenization/vocabulary.py
@@ -1,1 +1,31 @@
+from collections import Counter
 
+class Vocabulary:
+
+    def __init__(self):
+        self.token_to_id = {"PAD":0,"START":1,"END":2}
+        self.id_to_token = {0:"PAD",1:"START",2:"END"}
+
+    def build(self,token_lists):
+
+        counter = Counter()
+
+        for tokens in token_lists:
+            counter.update(tokens)
+
+        for token in counter:
+
+            if token not in self.token_to_id:
+
+                idx=len(self.token_to_id)
+
+                self.token_to_id[token]=idx
+                self.id_to_token[idx]=token
+
+    def encode(self,tokens):
+
+        return [self.token_to_id[t] for t in tokens]
+
+    def decode(self,ids):
+
+        return [self.id_to_token[i] for i in ids]
